429ec4de2a708bcae1012c5c840fd652cfeff8b8
Bhavesh Raj <bhaveshraj111@gmail.com>
2025-08-25 14:00:50 +0200

    on-board led setup

---
+++ main/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "esp_system.h"
+#include "driver/gpio.h"
+#include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led_strip.h"
+//#include "led_strip_rmt.h"
+
+#define BLINK_GPIO 48
+
+led_strip_handle_t led_strip;
+
+void init_led_strip(){
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = BLINK_GPIO,
+        .max_leds = 1
+    };
+    led_strip_rmt_config_t rmt_config = {
+        .resolution_hz = 10 * 1000 * 1000, // 10MHz
+        .flags.with_dma = false,
+    };
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+    ESP_ERROR_CHECK(led_strip_clear(led_strip)); // Clear the strip initially
+} 
+
+void set_led_color(int r, int g, int b, int timeout_ms, int blank_ms) {
+    ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, 0, r, g, b));
+    ESP_ERROR_CHECK(led_strip_refresh(led_strip));
+    vTaskDelay(pdMS_TO_TICKS(timeout_ms));
+    ESP_ERROR_CHECK(led_strip_clear(led_strip));
+    ESP_ERROR_CHECK(led_strip_refresh(led_strip));
+    vTaskDelay(pdMS_TO_TICKS(blank_ms));
+}
+
+void app_main(void){
+    init_led_strip();
+    while (1) {
+        set_led_color(0, 32, 0, 500, 500); // green
+    }
+}
